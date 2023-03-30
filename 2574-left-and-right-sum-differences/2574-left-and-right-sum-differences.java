bdd1319873fe6c43e4fa9abd57438729b96a0fd3
Mourya <125553814+MouryaPeddineni@users.noreply.github.com>
2023-03-30 14:24:33 +0530

    Time: 1 ms (100.00%), Space: 43.5 MB (20.40%) - LeetHub

--- 2574-left-and-right-sum-differences/2574-left-and-right-sum-differences.java
+++ 2574-left-and-right-sum-differences/2574-left-and-right-sum-differences.java
@@ -1,34 +1,17 @@
 class Solution {
     public int[] leftRigthDifference(int[] nums) {
-        // int[] answer = new int[nums.length];
-        int[] leftsum = new int[nums.length];
-        int[] rightsum = new int[nums.length];
-        for(int i=0;i<nums.length;i++){
-            leftsum[i] = leftsum_met(nums,i);
-            rightsum[i] = rightsum_met(nums,i);
-            if(leftsum[i]>rightsum[i]){
-                leftsum[i] = leftsum[i] - rightsum[i]; 
-            }
-            else{
-                leftsum[i] = rightsum[i] - leftsum[i];
-            }
-        }
-        return leftsum;
-    }
-    
-    public int leftsum_met(int[] nums,int n){
+        int n = nums.length;
+        int[] res = new int[n];
         int sum = 0;
-        for(int j=0;j<n;j++){
-            sum += nums[j];
+        for (int i = 0; i < n; i++) {
+            sum += nums[i];
+            res[i] = sum;
         }
-        return sum;
-    }
-    
-    public int rightsum_met(int []nums,int n){
-        int sum = 0;
-        for(int k=nums.length-1;k>n;k--){
-            sum += nums[k];
+        sum = 0;
+        for (int i = n - 1; i >= 0; i--) {
+            sum += nums[i];
+            res[i] = Math.abs(res[i] - sum);
         }
-        return sum;
+        return res;
     }
 }
