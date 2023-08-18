30372d52d97abe8f805037a672846a76e64eddca
Mourya <125553814+MouryaPeddineni@users.noreply.github.com>
2023-08-18 16:06:15 +0530

    Time: 3 ms (93.10%), Space: 13.4 MB (14.87%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -1,52 +1,50 @@
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
-        int m=grid.size(),n=grid[0].size();
-        int vis[m][n];
-        queue<pair<pair<int,int>,int>> q;
-        int cntrotting=0,countone;
+        int m = grid.size(),n = grid[0].size();
+        vector<vector<int>> vis(m, vector<int> (n,0));
+        //q{{r,c},t}
+        queue<pair<pair<int,int>,int>> bfs;
+        int cnt=0; //total number pf oranges(rotten and unrotten(1 or 2)).
+        int rotten=0; //total rotten oranges.
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(grid[i][j]==2){
-                    q.push({{i,j},0});
+                    rotten++;
                     vis[i][j]=1;
+                    bfs.push({{i,j},0});
                 }
-                else if(grid[i][j]==1){
-                    countone++;
-                    vis[i][j]=0;
-                }
-                else{
-                    vis[i][j]=0;
+                if(grid[i][j]!=0){ 
+                    cnt++;
                 }
             }
         }
         
-        int delrow[] = {-1,0,1,0};
-        int delcol[] = {0,1,0,-1};
-        int tm = 0;
+        int drow[] = {-1,0,1,0};
+        int dcol[] = {0,1,0,-1};
         
-        while(!q.empty()){
-            int r = q.front().first.first;
-            int c = q.front().first.second;
-            int t = q.front().second;
-            tm = max(tm,t);
-            q.pop();
+        int t=0;
+            
+        while(!bfs.empty()){
+            int srow = bfs.front().first.first;
+            int scol = bfs.front().first.second;
+            t = bfs.front().second;
+            bfs.pop();
+            // t=max(t,tm);
             for(int i=0;i<4;i++){
-                int nr = r+delrow[i];
-                int nc = c+delcol[i];
-                if(nr>=0 && nr<m && nc>=0 && nc<n && grid[nr][nc]==1 && !vis[nr][nc]){
-                    vis[nr][nc]=1;
-                    cntrotting++;
-                    q.push({{nr,nc},t+1});
+                int nrow = srow + drow[i];
+                int ncol = scol + dcol[i];
+                if(nrow>=0 && nrow<m && ncol>=0 && ncol<n && !vis[nrow][ncol]
+                   && grid[nrow][ncol]==1){
+                    rotten++;
+                    vis[nrow][ncol]=1;
+                    grid[nrow][ncol]=2;
+                    bfs.push({{nrow,ncol},t+1});
                 }
             }
         }
         
-        cout << cntrotting << " " << countone << endl;
-        
-        if(cntrotting==countone){
-            return tm;
-        }
+        if(rotten==cnt) return t;
         return -1;
     }
 };
