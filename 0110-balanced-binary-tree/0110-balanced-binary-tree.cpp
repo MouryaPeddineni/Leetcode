d060e94434eb7881c1be46756b40406325bece1a
Mourya <125553814+MouryaPeddineni@users.noreply.github.com>
2023-11-06 20:34:34 +0530

    Time: 3 ms (98.17%), Space: 21.5 MB (17.49%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
@@ -14,9 +14,10 @@ public:
     int height(TreeNode* root){
         if(root==NULL) return 0;
         int lh = height(root->left);
+        if(lh==-1) return -1;
         int rh = height(root->right);
+        if(rh==-1) return -1;
         if(abs(lh-rh)>1) return -1;
-        if(lh==-1 || rh==-1) return -1;
         return max(lh,rh)+1;
     }
 public:
