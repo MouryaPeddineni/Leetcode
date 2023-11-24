97fb2926552678629cfe16f4b9c8a9e4f083ce7b
Mourya <125553814+MouryaPeddineni@users.noreply.github.com>
2023-11-24 19:37:42 +0530

    Time: 2 ms (46.40%), Space: 6.2 MB (99.15%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -3,7 +3,7 @@ public:
     int climbStairs(int n) {
         int next1=1,next2=1;
         int curr=1;
-        for(int i=n-2;i>=0;i--){
+        for(int i=n;i>=2;i--){
             curr = next1+next2;
             next2=next1;
             next1=curr;
