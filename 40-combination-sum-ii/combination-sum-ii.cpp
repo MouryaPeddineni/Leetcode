ea0360ba64027d75e8fa3832b9e1d95f0cb9cf4c
Mourya <125553814+MouryaPeddineni@users.noreply.github.com>
2023-11-25 18:26:16 +0530

    Time: 0 ms (100.00%) | Memory: 13.8 MB (19.13%) - LeetSync

---
+++ 40-combination-sum-ii/combination-sum-ii.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    void combSum(int ind,int target,int n,vector<int> candidates,vector<int>& ds,vector<vector<int>>& ans){
+        if(target==0){
+            ans.push_back(ds);
+            return;
+        }
+        if(target<0) return;
+        for(int i=ind;i<n;i++){
+            if(i>ind && candidates[i]==candidates[i-1]) continue;
+            if(candidates[i]>target) break;
+            ds.push_back(candidates[i]);
+            combSum(i+1,target-candidates[i],n,candidates,ds,ans);
+            ds.pop_back();
+        }
+        return;
+    }
+public:
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        vector<vector<int>> ans;
+        sort(candidates.begin(),candidates.end());
+        int n = candidates.size();
+        vector<int> ds;
+        combSum(0,target,n,candidates,ds,ans);
+        return ans;
+    }
+};
