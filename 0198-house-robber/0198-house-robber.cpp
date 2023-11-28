560ec2dccb128410b18b1693e593d268e9b71ddb
Mourya <125553814+MouryaPeddineni@users.noreply.github.com>
2023-11-28 13:34:09 +0530

    Time: 0 ms (100.00%), Space: 8.1 MB (37.94%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,17 +1,24 @@
 class Solution {
-public:
-    int maxStole(int ind,vector<int> nums,vector<int>& dp){
-        if(ind==0) return nums[0];
-        if(ind<0) return 0;
-        if(dp[ind]!=-1) return dp[ind];
-        int take = nums[ind]+maxStole(ind-2,nums,dp);
-        int nottake = maxStole(ind-1,nums,dp);
-        return dp[ind]=max(take,nottake);
-    }
+// public:
+//     int maxStole(int ind,vector<int> nums,vector<int>& dp){
+//         if(ind==0) return nums[0];
+//         if(ind<0) return 0;
+//         if(dp[ind]!=-1) return dp[ind];
+//         int take = nums[ind]+maxStole(ind-2,nums,dp);
+//         int nottake = maxStole(ind-1,nums,dp);
+//         return dp[ind]=max(take,nottake);
+//     }
 public:
     int rob(vector<int>& nums) {
-        vector<int> dp(nums.size(),-1);
-        int ans = maxStole(nums.size()-1,nums,dp);
-        return ans;
+        int n = nums.size();
+        vector<int> dp(n,-1);
+        dp[0]=nums[0];
+        for(int i=1;i<n;i++){
+            int take = nums[i];
+            if(i>1) take+=dp[i-2];
+            int nottake = dp[i-1];
+            dp[i] = max(take,nottake);
+        }
+        return dp[n-1];
     }
 };
