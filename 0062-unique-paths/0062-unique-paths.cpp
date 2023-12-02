6f98d84c25365d05afd313f9016a2e28e48d883f
Mourya <125553814+MouryaPeddineni@users.noreply.github.com>
2023-12-03 01:05:16 +0530

    Time: 0 ms (100.00%), Space: 7 MB (9.44%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -3,9 +3,10 @@ public:
     int uniquePaths(int m, int n) {
         vector<vector<int>> dp(m, vector<int> (n,-1));
         for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(i==0 || j==0) dp[i][j]=1;
-            }
+            dp[i][0]=1;
+        }
+        for(int j=0;j<n;j++){
+            dp[0][j]=1;
         }
         for(int i=1;i<m;i++){
             for(int j=1;j<n;j++){
