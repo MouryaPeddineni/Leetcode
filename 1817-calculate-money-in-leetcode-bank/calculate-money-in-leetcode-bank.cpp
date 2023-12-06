5ae36bf02b34a6ad60ebdba5b4b760b34c867ab8
Mourya <125553814+MouryaPeddineni@users.noreply.github.com>
2023-12-07 00:48:21 +0530

    Time: 3 ms (13.68%) | Memory: 6.5 MB (15.79%) - LeetSync

--- 1817-calculate-money-in-leetcode-bank/calculate-money-in-leetcode-bank.cpp
+++ 1817-calculate-money-in-leetcode-bank/calculate-money-in-leetcode-bank.cpp
@@ -1,13 +1,19 @@
 class Solution {
 public:
     int totalMoney(int n) {
-        vector<int> dp(n,-1);
-        dp[0]=1;
         int sum = 1;
+        int prev = 1, curr = 0, prev7 = 1;
         for(int i=1;i<n;i++){
-            if(i<7) dp[i]=1+dp[i-1];
-            else dp[i]=dp[i-7]+1;
-            sum+=dp[i];
+            if(i<7) curr = 1+prev;
+            else{
+                if(i%7==0){
+                    curr = 1 + prev7;
+                    prev7 = curr;
+                }
+                else curr = prev+1;
+            }
+            sum += curr;
+            prev = curr;
         }
         return sum;
     }
