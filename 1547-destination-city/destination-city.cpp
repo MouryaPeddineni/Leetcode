b96d4972dc94a64ba9c2d46fe5138b5c4a6ad24b
Mourya <125553814+MouryaPeddineni@users.noreply.github.com>
2023-12-15 19:18:43 +0530

    Time: 12 ms (35.41%) | Memory: 11.8 MB (23.80%) - LeetSync

--- 1547-destination-city/destination-city.cpp
+++ 1547-destination-city/destination-city.cpp
@@ -1,15 +1,11 @@
 class Solution {
 public:
-    string destCity(vector<vector<string>>& paths) {
-        map<string,int> mpp;
-        string ans = "";
-        for(int i=0;i<paths.size();i++){
-            mpp[paths[i][0]]++;
-            mpp[paths[i][1]]--;
-        }
-        for(auto it:mpp){
-            if(it.second==-1) ans = it.first;
-        }
-        return ans;
+  string destCity(vector<vector<string>>& paths) {                
+        unordered_set<string> hash;
+        for(auto e: paths)  
+            hash.insert(e[0]);
+        for(auto e: paths) 
+            if(hash.find(e[1]) == hash.end()) return e[1];             
+        return "";
     }
 };
