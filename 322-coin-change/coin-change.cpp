201e8d5cef18d0d69844e10219e27581f6c2bb49
Mourya <125553814+MouryaPeddineni@users.noreply.github.com>
2023-12-15 18:43:31 +0530

    Time: 75 ms (53.52%) | Memory: 19.1 MB (35.67%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -2,23 +2,22 @@ class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
         int n = coins.size();
-        vector<vector<int>> dp(n, vector<int> (amount+1, 0));
-        for(int i=0;i<n;i++){
-            dp[i][0] = 0;
-        }
+        vector<int> prev(amount+1,0), cur(amount+1,0);
+        prev[0] = cur[0] = 0;
         for(int t=0;t<=amount;t++){
-            if(t%coins[0]==0) dp[0][t] = t/coins[0];
-            else dp[0][t] = 1e8;
+            if(t%coins[0]==0) prev[t] = t/coins[0];
+            else prev[t] = 1e8;
         }
         for(int i=1;i<n;i++){
             for(int j=1;j<=amount;j++){
-                int notpick = dp[i-1][j];
+                int notpick = prev[j];
                 int pick = 1e8;
-                if(coins[i]<=j) pick = 1 + dp[i][j-coins[i]];
-                dp[i][j] = min(pick,notpick);
+                if(coins[i]<=j) pick = 1 + cur[j-coins[i]];
+                cur[j] = min(pick,notpick);
             }
+            prev = cur;
         }
-        int ans = dp[n-1][amount];
+        int ans = prev[amount];
         if(ans>=1e8) return -1;
         return ans;
     }
