c929d58dd8fd0b84d3ea4169febe67bc34ee166f
Mourya <125553814+MouryaPeddineni@users.noreply.github.com>
2023-12-15 17:56:50 +0530

    Time: 81 ms (45.48%) | Memory: 45.9 MB (25.47%) - LeetSync

---
+++ 322-coin-change/coin-change.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int f(int i,int target,vector<int>& coins,vector<vector<int>>& dp){
+        if(target <= 0) return 0;
+        if(i==0){
+            if(target%coins[0]==0) return target/coins[0];
+            else return 1e8;
+        }
+        if(dp[i][target]!=-1) return dp[i][target];
+        int notpick = f(i-1,target,coins,dp);
+        int pick = 1e8;
+        if(coins[i]<=target) pick = 1 + f(i,target-coins[i],coins,dp);
+        return dp[i][target] = min(pick,notpick);
+    }
+
+    int coinChange(vector<int>& coins, int amount) {
+        int n = coins.size();
+        vector<vector<int>> dp(n, vector<int> (amount+1, -1));
+        int ans = f(n-1,amount,coins,dp);
+        if(ans>=1e8) return -1;
+        return ans;
+    }
+};
