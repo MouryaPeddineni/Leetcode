cdb18fc51a7599e573a45a62f8680c49211869b1
Mourya <125553814+MouryaPeddineni@users.noreply.github.com>
2023-12-18 18:56:25 +0530

    Time: 19 ms (83.95%) | Memory: 20.6 MB (64.73%) - LeetSync

--- 2042-maximum-product-difference-between-two-pairs/maximum-product-difference-between-two-pairs.cpp
+++ 2042-maximum-product-difference-between-two-pairs/maximum-product-difference-between-two-pairs.cpp
@@ -1,27 +1,31 @@
 class Solution {
 public:
     int maxProductDifference(vector<int>& nums) {
-        int biggest = 0;
-        int secondBiggest = 0;
-        int smallest = INT_MAX;
-        int secondSmallest = INT_MAX;
-        
-        for (int num : nums) {
-            if (num > biggest) {
-                secondBiggest = biggest;
-                biggest = num;
-            } else {
-                secondBiggest = max(secondBiggest, num);
+        int maxi = nums[0];
+        int largestIndex = 0,smallestIndex=0;
+        int secondMaxi = INT_MIN;
+        int mini = nums[0];
+        int secondMini = INT_MAX;
+        int n = nums.size();
+        for(int i=0;i<n;i++){
+            if(nums[i]>maxi){
+                maxi = nums[i];
+                largestIndex = i;
             }
-            
-            if (num < smallest) {
-                secondSmallest = smallest;
-                smallest = num;
-            } else {
-                secondSmallest = min(secondSmallest, num);
+            if(nums[i]<mini){
+                mini = nums[i];
+                smallestIndex = i;
             }
         }
-        
-        return biggest * secondBiggest - smallest * secondSmallest;
+        for(int i=0;i<n;i++){
+            if(nums[i]<=maxi && nums[i]>secondMaxi && i!=largestIndex){
+                secondMaxi = nums[i];
+            }
+            if(nums[i]>=mini && nums[i]<secondMini && i!=smallestIndex){
+                secondMini = nums[i];
+            }
+        }
+        cout << largestIndex << "->" << maxi << " " << smallestIndex <<"->" << mini << endl;
+        return maxi*secondMaxi - mini*secondMini;
     }
 };
