843249903733439f950ba4da61a743a2bb4bd070
Mourya <125553814+MouryaPeddineni@users.noreply.github.com>
2023-12-26 19:39:00 +0530

    Time: 34 ms (35.65%) | Memory: 18.6 MB (61.52%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.cpp
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -1,22 +1,41 @@
 class Solution {
-public:
-    int f(int ind, int amt, vector<int>& coins, vector<vector<int>>& dp){
-        if(amt == 0) return 1;
-        if(amt < 0) return 0;
-        if(ind == 0){
-            if(amt%coins[0]==0) return 1;
-            return 0;
-        }
-        if(dp[ind][amt]!=-1) return dp[ind][amt];
-        int pick = 0;
-        if(coins[ind]<=amt) pick = f(ind, amt-coins[ind], coins, dp);
-        int notpick = f(ind-1, amt, coins, dp);
-        return dp[ind][amt] = pick + notpick;
-    }
+// public:
+//     int f(int ind, int amt, vector<int>& coins, vector<vector<int>>& dp){
+//         if(amt == 0) return 1;
+//         if(amt < 0) return 0;
+//         if(ind == 0){
+//             if(amt%coins[0]==0) return 1;
+//             return 0;
+//         }
+//         if(dp[ind][amt]!=-1) return dp[ind][amt];
+//         int pick = 0;
+//         if(coins[ind]<=amt) pick = f(ind, amt-coins[ind], coins, dp);
+//         int notpick = f(ind-1, amt, coins, dp);
+//         return dp[ind][amt] = pick + notpick;
+//     }
 public:
     int change(int amount, vector<int>& coins) {
         int n = coins.size();
-        vector<vector<int>> dp(n, vector<int> (amount+1, -1));
-        return f(n-1, amount, coins, dp);
+        vector<vector<int>> dp(n, vector<int> (amount+1, 0));
+        for(int i=0;i<=amount;i++){
+            if(i%coins[0]==0) dp[0][i] = 1;
+        }
+        for(int i=1;i<n;i++){
+            for(int j=0;j<=amount;j++){
+                int pick = 0;
+                if(coins[i]<=j) pick = dp[i][j-coins[i]];
+                int notpick = dp[i-1][j];
+                dp[i][j] = pick + notpick;
+            }
+        }
+
+        // for(int i=0;i<n;i++){
+        //     for(int j=0;j<=amount;j++){
+        //         cout << dp[i][j] << " ";
+        //     }
+        //     cout << endl;
+        // }
+        // return f(n-1, amount, coins, dp);
+        return dp[n-1][amount];
     }
 };
