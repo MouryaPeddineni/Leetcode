d6d34b70fff9f2ba54d9d8f4738d55bb5263a19c
Mourya <125553814+MouryaPeddineni@users.noreply.github.com>
2023-12-30 12:10:50 +0530

    Time: 22 ms (12.84%) | Memory: 12 MB (68.09%) - LeetSync

--- 2025-redistribute-characters-to-make-all-strings-equal/redistribute-characters-to-make-all-strings-equal.cpp
+++ 2025-redistribute-characters-to-make-all-strings-equal/redistribute-characters-to-make-all-strings-equal.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     bool makeEqual(vector<string>& words) {
         int n = words.size();
-        unordered_map<char,int> mpp;
+        map<char,int> mpp;
         for(int i=0;i<words.size();i++){
             for(int j=0;j<words[i].size();j++){
                 mpp[words[i][j]]++;
