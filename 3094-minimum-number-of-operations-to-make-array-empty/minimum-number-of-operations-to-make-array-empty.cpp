e8745609fdca934e17db53f280f9f8920cf0afae
Mourya <125553814+MouryaPeddineni@users.noreply.github.com>
2024-01-04 21:34:20 +0530

    Time: 130 ms (34.25%) | Memory: 85.7 MB (14.00%) - LeetSync

---
+++ 3094-minimum-number-of-operations-to-make-array-empty/minimum-number-of-operations-to-make-array-empty.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int minOperations(vector<int>& nums) {
+        map<int,int> mpp;
+        int n = nums.size();
+        for(int i=0;i<n;i++){
+            mpp[nums[i]]++;
+        }
+        int totOps = 0;
+        for(auto it:mpp){
+            if(it.second==1) return -1;
+            if((it.second)%3==0) totOps += (it.second)/3;
+            else if((it.second)%3==1) totOps += (it.second-4)/3 + 2;
+            else totOps += (it.second)/3 + 1;
+        }
+        return totOps;
+    }
+};
