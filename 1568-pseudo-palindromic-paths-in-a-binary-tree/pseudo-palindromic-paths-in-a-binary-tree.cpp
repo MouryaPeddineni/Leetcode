6208cbb8f8765da72928ee3a8641569f6029b70b
Mourya <125553814+MouryaPeddineni@users.noreply.github.com>
2024-01-24 11:56:24 +0530

    Time: 316 ms (33.51%) | Memory: 177.9 MB (98.12%) - LeetSync

---
+++ 1568-pseudo-palindromic-paths-in-a-binary-tree/pseudo-palindromic-paths-in-a-binary-tree.cpp
@@ -0,0 +1,46 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int pseudo(TreeNode* node, map<int,int> &mpp){
+        if(node==nullptr){
+            return 0;
+        }
+        if(node->left==nullptr && node->right==nullptr){
+            mpp[node->val]++;
+            int odds = 0;
+            for(auto it:mpp){
+                // cout << it.first << " " << it.second << "\n";
+                // sum+=it.second;
+                if(it.second%2!=0) odds++;
+            }
+            cout << "\n";
+            // mpp[node->val]--;
+            if(odds==1) return 1;
+            if(odds==0) return 1;
+            return 0;
+        }
+        mpp[node->val]++;
+        int left = pseudo(node->left, mpp);
+        if(node->left!=nullptr) mpp[node->left->val]--;
+        int right = pseudo(node->right, mpp);
+        if(node->right!=nullptr) mpp[node->right->val]--;
+        return left + right;
+    }
+public:
+    int pseudoPalindromicPaths (TreeNode* root) {
+        if(root->left==nullptr && root->right==nullptr) return 1;
+        map<int,int> mpp;
+        int ans = pseudo(root, mpp);
+        return ans;
+    }
+};
