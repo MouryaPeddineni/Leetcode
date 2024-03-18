9c04e3299865d2f222e0ef1d11b49d65461a43d7
Mourya <125553814+MouryaPeddineni@users.noreply.github.com>
2024-03-18 19:02:48 +0530

    Time: 16 ms (63.67%) | Memory: 22.3 MB (7.31%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
@@ -9,35 +9,41 @@
  * };
  */
 
-struct CompareNodes {
-    bool operator()(ListNode* a, ListNode* b) {
-        return a->val > b->val;
-    }
-};
-
-
 class Solution {
 public:
     ListNode* mergeKLists(vector<ListNode*>& lists) {
         if (lists.empty()) return nullptr;
+        if (lists.size() == 1) return lists[0];
         
-        std::priority_queue<ListNode*, std::vector<ListNode*>, CompareNodes> minHeap;
+        return mergeLists(lists, 0, lists.size() - 1);
+    }
+
+    ListNode* mergeLists(std::vector<ListNode*>& lists, int left, int right) {
+        if (left == right) return lists[left];
+        
+        int mid = left + (right - left) / 2;
+        ListNode* l = mergeLists(lists, left, mid);
+        ListNode* r = mergeLists(lists, mid + 1, right);
+        
+        return merge(l, r);
+    }
+
+    ListNode* merge(ListNode* l, ListNode* r) {
         ListNode* dummy = new ListNode(0);
         ListNode* tail = dummy;
         
-        for (ListNode* node : lists) {
-            if (node != nullptr)
-                minHeap.push(node);
+        while (l && r) {
+            if (l->val < r->val) {
+                tail->next = l;
+                l = l->next;
+            } else {
+                tail->next = r;
+                r = r->next;
+            }
+            tail = tail->next;
         }
         
-        while (!minHeap.empty()) {
-            tail->next = minHeap.top();
-            minHeap.pop();
-            tail = tail->next;
-            
-            if (tail->next != nullptr)
-                minHeap.push(tail->next);
-        }
+        tail->next = l ? l : r;
         
         return dummy->next;
     }
