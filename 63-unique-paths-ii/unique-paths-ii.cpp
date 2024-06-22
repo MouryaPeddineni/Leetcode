39a246ca4a5a63dd84a93eaa5ae66640dd12613b
Mourya <125553814+MouryaPeddineni@users.noreply.github.com>
2024-06-22 10:27:32 +0530

    Time: 0 ms (100.00%) | Memory: 10 MB (80.87%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.cpp
+++ 63-unique-paths-ii/unique-paths-ii.cpp
@@ -4,11 +4,12 @@ public:
         int n = obstacleGrid.size(), m = obstacleGrid[0].size();
         // vector<vector<int>> dp(n+1, vector<int> (m+1, 0));
         vector<int> prev(m+1, 0);
+        vector<int> curr(m+1, 0);
         prev[1] = 1;
         for(int i=1;i<=n;i++) {
-            vector<int> curr(m+1, 0);
             for(int j=1;j<=m;j++) {
                 if(obstacleGrid[i-1][j-1]!=1) curr[j] = prev[j] + curr[j-1];
+                else curr[j] = 0;
             }
             prev = curr;
         }
