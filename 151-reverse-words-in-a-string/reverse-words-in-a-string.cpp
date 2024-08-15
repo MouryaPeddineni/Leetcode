124ee577635a1161f1b1588fbc0aeba07c4e9c0c
Mourya <125553814+MouryaPeddineni@users.noreply.github.com>
2024-08-15 10:31:54 +0530

    Time: 3 ms (81.16%) | Memory: 8.8 MB (75.11%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
@@ -1,24 +1,33 @@
 class Solution {
 public:
     string reverseWords(string s) {
-        stack<string> st;
         int n = s.size();
-        int i=0;
-        while(i<n && s[i]==' ') i++;
-        while(i<n) {
-            string dummy = "";
-            while(i<n && s[i] != ' ') {
-                dummy += s[i];
-                i++;
-            }
-            while(i<n && s[i]==' ') i++;
-            st.push(dummy);
+        reverseString(s, 0, n-1);
+        reverseWord(s);
+        cleanSpaces(s, n);
+        return s;
+    }
+    void reverseString(string &s, int i, int j) {
+        while(i<j) swap(s[i++], s[j--]);
+    }
+    void reverseWord(string &s) {
+        int n = s.size();
+        int i=0, j=0;
+        while(i<n && j<n) {
+            while(i<j || i<n && s[i]==' ') i++;
+            while(j<i || j<n && s[j] != ' ') j++;
+            reverseString(s, i, j-1);
+            i = j;
         }
-        string ans = "";
-        while(!st.empty()) {
-            ans += st.top() + " ";
-            st.pop();
+    }
+    void cleanSpaces(string &s, int n){
+        int i = 0, j = 0;
+        while (j < n) {
+            while (j < n && s[j] == ' ') j++;             
+            while (j < n && s[j] != ' ') s[i++] = s[j++]; 
+            while (j < n && s[j] == ' ') j++;             
+            if (j < n) s[i++] = ' ';                      
         }
-        return ans.substr(0, ans.size()-1);
+        s = s.substr(0, i);
     }
 };
