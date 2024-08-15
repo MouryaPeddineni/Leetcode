a57e82474719839fb3163f211249d2ef6cc8bd23
Mourya <125553814+MouryaPeddineni@users.noreply.github.com>
2024-08-15 23:03:44 +0530

    Time: 3 ms (89.25%) | Memory: 16.4 MB (26.66%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,34 +1,43 @@
 class Solution {
 public:
+    int first(vector<int>& nums, int low, int high, int a, int target) {
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(nums[mid]==target) {
+                a = mid;
+                high = mid-1;
+            }
+            else if(nums[mid] < target) {
+                low = mid+1;
+            }
+            else high = mid-1;
+        }
+        return a;
+    }
+public:
+    int last(vector<int>& nums, int low, int high, int b, int target) {
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(nums[mid]==target) {
+                b = mid;
+                low = mid+1;
+            }
+            else if(nums[mid] < target) {
+                low = mid+1;
+            }
+            else high = mid-1;
+        }
+        return b;
+    }
+public:
     vector<int> searchRange(vector<int>& nums, int target) {
         vector<int> ans(2);
         int n = nums.size();
         int a = -1, b = -1;
         int low = 0, high = n-1, mid;
-        while(low<=high){
-            mid=low+(high-low)/2;
-            if(nums[mid]<=target) {
-                if(nums[mid]==target) a = mid;
-                low = mid+1;
-            }
-            else {
-                high = mid-1;
-            }
-        }
-        ans[1] = a;
-        low = 0;
-        high = n-1;
-        while(low<=high){
-            mid=low+(high-low)/2;
-            if(nums[mid]>=target) {
-                if(nums[mid]==target) b = mid;
-                high = mid-1;
-            }
-            else {
-                low = mid+1;
-            }
-        }
-        ans[0] = b;
+        ans[0] = first(nums, low, high, a, target);
+        if(ans[0] == -1) return {-1, -1};
+        ans[1] = last(nums, low, high, b, target);
         return ans;
     }
 };
