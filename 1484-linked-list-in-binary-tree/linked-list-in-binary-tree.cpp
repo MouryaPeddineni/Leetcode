9dcf6663495d27599e1d1e5c58bef4afb4b8a7f8
Mourya <125553814+MouryaPeddineni@users.noreply.github.com>
2024-09-07 19:16:25 +0530

    Time: 30 ms (24.71%) | Memory: 30.8 MB (36.50%) - LeetSync

--- 1484-linked-list-in-binary-tree/linked-list-in-binary-tree.cpp
+++ 1484-linked-list-in-binary-tree/linked-list-in-binary-tree.cpp
@@ -22,14 +22,25 @@
 class Solution {
 public:
     bool isSubPath(ListNode* head, TreeNode* root) {
-        if (!head) return true;
-        if (!root) return false;
-        return dfs(head, root) || isSubPath(head, root->left) || isSubPath(head, root->right);
+        vector<int> A = {head->val}, dp = {0};
+        int i = 0;
+        head = head->next;
+        while (head) {
+            while (i && head->val != A[i])
+                i = dp[i - 1];
+            i += head->val == A[i];
+            A.push_back(head->val);
+            dp.push_back(i);
+            head = head->next;
+        }
+        return dfs(root, 0, A, dp);
     }
 
-    bool dfs(ListNode* head, TreeNode* root) {
-        if (!head) return true;
+    bool dfs(TreeNode* root, int i, vector<int>& A, vector<int>& dp) {
         if (!root) return false;
-        return head->val == root->val && (dfs(head->next, root->left) || dfs(head->next, root->right));
+        while (i && root->val != A[i])
+            i = dp[i - 1];
+        i += root->val == A[i];
+        return i == dp.size() || dfs(root->left, i, A, dp) || dfs(root->right, i, A, dp);
     }
 };
