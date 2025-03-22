26a51f38d7456d9039ad15c7272851e447237fb5
Mourya <125553814+MouryaPeddineni@users.noreply.github.com>
2025-03-22 08:41:56 +0530

    Time: 70 ms (5.10%) | Memory: 77.3 MB (23.93%) - LeetSync

--- 1916-find-center-of-star-graph/find-center-of-star-graph.java
+++ 1916-find-center-of-star-graph/find-center-of-star-graph.java
@@ -3,15 +3,9 @@ class Solution {
         int n = edges.length;
         int cntHighest = 0, node = 0;
         Map<Integer, Integer> mpp = new HashMap<>();
-        for(int i=0;i<n;i++) {
-            if(mpp.containsKey(edges[i][0])) 
-                mpp.put(edges[i][0], mpp.get(edges[i][0])+1);
-            else
-                mpp.put(edges[i][0], 1);
-            if(mpp.containsKey(edges[i][1]))
-                mpp.put(edges[i][1], mpp.get(edges[i][1])+1);
-            else
-                mpp.put(edges[i][1], 1);
+        for(int i=0;i<n;i++) {    
+            mpp.put(edges[i][0], 1 + mpp.getOrDefault(edges[i][0], 0));
+            mpp.put(edges[i][1], 1 + mpp.getOrDefault(edges[i][1], 0));
             if(mpp.get(edges[i][0]) > cntHighest) {
                 node = edges[i][0];
                 cntHighest = mpp.get(edges[i][0]);
