954df8edbc3b869500f4061a80cf47fb412a155c
Mourya <125553814+MouryaPeddineni@users.noreply.github.com>
2025-03-30 09:47:00 +0530

    Time: 47 ms (12.99%) | Memory: 29.7 MB (39.90%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.cpp
+++ 44-wildcard-matching/wildcard-matching.cpp
@@ -1,50 +1,36 @@
 class Solution {
 public:
-    bool f(int i, int j, string pattern, string text, vector<vector<int>> &dp){
-        if(i==0 && j==0) return true;
-        if(i==0 && j>0) return false;
-        if(j==0 && i>0){
-            for(int k=1;k<=i;k++){
-                if(pattern[k-1]!='*') return false;
-            }
-            return true;
-        }
-        if(dp[i][j]!=-1) return dp[i][j];
-        if(pattern[i-1]!='*' && pattern[i-1]!='?' && pattern[i-1]!=text[j-1]) return false;
-        bool first = false, second = false;
-        if(pattern[i-1]==text[j-1] || pattern[i-1]=='?'){
-            first = f(i-1, j-1, pattern, text, dp);
-        }
-        if(pattern[i-1]=='*'){
-            second = f(i, j-1, pattern, text, dp) || f(i-1, j, pattern, text, dp); 
-        }
-        return dp[i][j] = first || second;
-    }
-public:
     bool isMatch(string text, string pattern) {
         int n = text.size(), m = pattern.size();
-        vector<vector<bool>> dp(m+1, vector<bool> (n+1, false));
-        dp[0][0] = true;
-        for(int i=1;i<=m;i++){
-            bool fl = true;
-            for(int k=1;k<=i;k++){
-                if(pattern[k-1]!='*') fl = false;
-            }
-            dp[i][0] = fl;
-        }
-
-        for(int i=1;i<=m;i++){
-            for(int j=1;j<=n;j++){
-                if(pattern[i-1]==text[j-1] || pattern[i-1]=='?'){
-                    dp[i][j] = dp[i-1][j-1];
+        // vector<vector<int>> dp(n+1, vector<int> (m+1,-1));
+        vector<int> prev(m+1, -1);
+        for(int i=0;i<=n;i++) {
+            vector<int> curr(m+1, -1);
+            for(int j=0;j<=m;j++) {
+                bool first = false, second = false;
+                if(i==0 && j==0) curr[0] = 1;
+                else if(j == 0) curr[0] = 0;
+                else if(i == 0) {
+                    for(int k=1;k<=j;k++){
+                        if(pattern[k-1]!='*') {
+                            curr[j] = 0;
+                            break;
+                        }
+                    }
+                    if(curr[j] == -1) curr[j] = 1;
                 }
-                else if(pattern[i-1]=='*'){
-                    dp[i][j] = dp[i][j-1] | dp[i-1][j]; 
+                else if(pattern[j-1]!='*' && pattern[j-1]!='?' && pattern[j-1]!=text[i-1]) curr[j] = 0;
+                else if(pattern[j-1]==text[i-1] || pattern[j-1]=='?'){
+                    first = prev[j-1];
                 }
-                else dp[i][j] = false;
+                else if(pattern[j-1]=='*'){
+                    second = curr[j-1] | prev[j];
+                }
+                if(curr[j]==-1) curr[j] = first | second;
             }
+            prev = curr;
         }
-
-        return dp[m][n];
+        if(prev[m] == 0) return false;
+        return true;
     }
 };
